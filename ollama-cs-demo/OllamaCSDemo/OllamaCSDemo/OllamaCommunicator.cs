cc85bf11874d7a420750f261c2be596e1d24da71
Doyoon Kim <dkim@roto.com>
2024-06-07 11:53:09 -0400

    Ollama CS
    Basic interactive chatting demo

---
+++ ollama-cs-demo/OllamaCSDemo/OllamaCSDemo/OllamaCommunicator.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OllamaCSDemo
+{
+    class OllamaCommunicator
+    {
+        private string model;
+        private string setupPrompt;
+
+        public OllamaCommunicator(string model, string setupPrompt)
+        {
+            this.model = model;
+            this.setupPrompt = setupPrompt;
+        }
+
+        public async Task<string> GenerateCompletionAsync(string model, string prompt)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var requestBody = new
+                {
+                    model = model,
+                    prompt = prompt
+                };
+                var json = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("http://localhost:11434/api/generate", content);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // combine multiple responses into one
+                var responses = responseBody.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                var finalResponse = new StringBuilder();
+
+                foreach (var res in responses)
+                {
+                    var jsonResponse = JObject.Parse(res);
+                    finalResponse.Append(jsonResponse["response"].ToString());
+                }
+
+
+                return finalResponse.ToString();
+            }
+        }
+
+        public async Task<string> ListLocalModelsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost:11434/api/list");
+                response.EnsureSuccessStatusCode();
+                
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return responseBody;
+            }
+        }
+    }
+}
---
+++ ollama-cs-demo/OllamaCSDemo/OllamaCSDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OllamaCSDemo;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        OllamaCommunicator ollaCommunicator = new OllamaCommunicator("llama3","You are a virtual assistant who gives me whatever answer I want.");
+
+        //get input from user until user types "exit"
+        string input = "";
+        while (input != "exit")
+        {
+            Console.WriteLine("Enter a prompt or type 'exit' to quit:");
+            input = Console.ReadLine();
+            if (input != "exit")
+            {
+                string response = await ollaCommunicator.GenerateCompletionAsync("llama3", input);
+                Console.WriteLine(response);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
